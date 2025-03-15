edc10e23c6e05e1e3949f3967c6731d22948f772
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2025-03-15 07:36:37 +0530

    Backedb Morgan Middleware Added

--- Mirror_Web/Backend/src/app.js
+++ Mirror_Web/Backend/src/app.js
@@ -5,15 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const morgan = require("morgan");
 // const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const textEmotionRoute = require("./routes/textEmotion.route");
 const userRoute = require("./routes/user.route");
 
 dbConnect();
 
+app.use(cors());
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 // app.use(notFound);
 // app.use(errorHandler);
 
