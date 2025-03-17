cd9b19fd72aca4509695a12eddc122b675fbe79d
djd <dananjayahbi@gmail.com>
2025-03-17 23:37:19 +0530

    mirror issue

--- Mirror_Web/Backend/src/routes/contact.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllTextEmotions,getEmotionsByTimestampFilter } = require("../controllers/contact.controller");
-
-//Get All Text Emotions An User
-router.post("/readContact", getAllTextEmotions);
-
-
-module.exports = router;
--- Mirror_Web/Backend/src/routes/textEmotion.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllTextEmotions,getEmotionsByTimestampFilter } = require("../controllers/textEmotionController");
-
-//Get All Text Emotions An User
-router.get("/textemotions", getAllTextEmotions);
-router.get("/textemotions-time", getEmotionsByTimestampFilter);
-
-module.exports = router;
